3cbf709e849fbbbb35a372820ee1f80c27f16657
ThePlatypusEveryoneLoves <atharv.sondhi@gmail.com>
2023-06-21 16:54:47 -0500

    Started the project

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,6 @@ std::string ConvertToRpn(std::string input){
 	std::vector<double> numbers;
 	std::vector<char> Operations;
 	while(input.length()){
-		std::cout<<input<<"\n"<<EndResult<<"\n";
 		char cur_char = input.at(0);
 		if(std::isdigit(cur_char)){
 			try{
@@ -46,21 +45,17 @@ std::string ConvertToRpn(std::string input){
 				std::cerr<<"Error happened while turing into number";
 			} 
 		} else if (is_in(cur_char, {'*','+','-','/'})) {
-		//	std::cout<<cur_char<<"\n";
 			if (Operations.size()&&static_cast<int>(OperatorMap.at(Operations.back()))>static_cast<int>(OperatorMap.at(cur_char))) {
 				char Op = Operations.back();
 				Operations.pop_back();
 				Operations.push_back(cur_char);
 				EndResult.append(1,Op);
-				std::cout<<input<<"\n";
 				input.erase(0,1);
-				std::cout<<input<<"\n";
 			} else {
 				Operations.push_back(cur_char);
 				input.erase(0,1);	
 			}
 		} else if (std::isspace(cur_char)) {
-				std::cout<<"space";
 				input.erase(0,1);	
 		} else {
 			throw std::invalid_argument(""); //TODO: write error message
@@ -71,7 +66,5 @@ std::string ConvertToRpn(std::string input){
 }
 
 int main(){
-	std::cout<<"Im here\n";
 	std::cout<<ConvertToRpn("3 + 4 * 2");
-//	std::cout<<isdigit('3');
 }
